ce95fde6cc61257202d7e6b7fab98967b071e3a1
Syahra Rizkia Andina <113167559+syahrarizkia@users.noreply.github.com>
2022-11-11 12:56:40 +0700

    Add files via upload

---
+++ package/perhitunganpajakkendaraanbermotor/src/PajakUtil.php
@@ -0,0 +1,20 @@
+<?php 
+
+    class PajakUtil {
+        public static function hitungPKBMobil(float $nilaiJualKendaraan): float {
+            return $nilaiJualKendaraan * 0.02;
+        }
+
+        public static function hitungBBNKBMobil(float $hargaJualKendaraan): float {
+            return $hargaJualKendaraan * 0.1;
+        }
+
+        public static function hitungPKBMotor(int $nilaiJualMotor): float {
+            return $nilaiJualMotor * 0.02;
+        }
+
+        public static function hitungBBNKBMotor(int $hargaJualMotor): float {
+            return $hargaJualMotor * 0.02;
+        }
+    }
+?>
