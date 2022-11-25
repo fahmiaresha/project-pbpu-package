ebb6004f2c1f2c98a1494c57294efa5c260eeba9
nurizzahmr <106067465+nurizzahmr@users.noreply.github.com>
2022-11-25 18:05:44 +0700

    Add files via upload

---
+++ package/perhitunganpajakkendaraanbermotor/src/PajakTahunPertamaMotor.php
@@ -0,0 +1,28 @@
+<?php 
+
+class MenghitungPajakTahunPertamaMotor implements Pajak{
+    private $jumlahPajak;
+    private $nilaiJualMotor;
+    private $swdkllj;
+    private $biayaAdministrasi;
+    private $biayaAdministrasiPenerbitanSTNK;
+
+    public function __construct(float $nilaiJualMotor, float $swdkllj, float $biayaAdministrasi, float $biayaAdministrasiPenerbitanSTNK) {
+        $this->nilaiJualMotor = $nilaiJualMotor;
+        $this->swdkllj = $swdkllj;
+        $this->biayaAdministrasi = $biayaAdministrasi;
+        $this->biayaAdministrasiPenerbitanSTNK = $biayaAdministrasiPenerbitanSTNK;
+    }
+
+    public function setPajakTahunPertama() {
+        $bbnkb = PajakUtil::hitungBBNKBMotor($this -> nilaiJualMotor);
+        $pkb = PajakUtil::hitungPKBMotor($this -> nilaiJualMotor);
+        $this -> jumlahPajak = $bbnkb + $pkb + $this -> swdkllj + $this -> biayaAdministrasi + $this -> biayaAdministrasiPenerbitanSTNK;
+    }
+
+    public function getJumlahPajak(): float {
+        return $this->jumlahPajak;
+    }
+}
+
+?>
