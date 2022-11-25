f90f70942368572de0406e4260138a38326a454f
F.A <faiz.al.qrn@gmail.com>
2022-11-25 10:03:16 +0700

    change name class

--- package/perhitunganpajakkendaraanbermotor/src/MelihatInformasiPajakKendaraanFactory.php
+++ package/perhitunganpajakkendaraanbermotor/src/MelihatInformasiPajakKendaraanFactory.php
@@ -1,26 +1,28 @@
 <?php
 
-class MelihatInformasiPajakKendaraanFactory extends PerhitunganPajakKendaraanAbstractFactory {
-    
-    // public function __construct() {
-    // }
+class MelihatInformasiPajakKendaraanFactory extends PajakKendaraanAbstractFactory
+{
 
-    public function getInformasi(string $informasi) {
-        if (strcasecmp($informasi, 'informasipresentasepajak') == 0 ) {
-            return new InformasiPresentasePajak();
-        }
-        elseif(strcasecmp($informasi, 'informasibiayaadministrasi') == 0){
-            return new InformasiBiayaAdministrasi();
-        }
-        return null;
-    }
+  // public function __construct() {
+  // }
 
-    public function getPajak(string $pajak) {
-        return null;
+  public function getInformasi(string $informasi)
+  {
+    if (strcasecmp($informasi, 'informasipresentasepajak') == 0) {
+      return new InformasiPresentasePajak();
+    } elseif (strcasecmp($informasi, 'informasibiayaadministrasi') == 0) {
+      return new InformasiBiayaAdministrasi();
     }
+    return null;
+  }
 
-    public function getDenda(string $denda) {
-        return null;
-    }
-   
+  public function getPajak(string $pajak)
+  {
+    return null;
+  }
+
+  public function getDenda(string $denda)
+  {
+    return null;
+  }
 }
