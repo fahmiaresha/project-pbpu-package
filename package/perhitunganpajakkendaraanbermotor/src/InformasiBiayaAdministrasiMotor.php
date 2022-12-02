926416d6c223b9b574a886970ab9717ad7e6db42
fahmiaresha <50320885+fahmiaresha@users.noreply.github.com>
2022-12-02 11:14:33 +0700

    move folder informasi

---
+++ package/perhitunganpajakkendaraanbermotor/src/InformasiBiayaAdministrasiMotor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace PerhitunganPajakKendaranBermotor;
+
+class InformasiBiayaAdministrasiMotor implements Informasi
+{
+  private $InformasiBiayaAdministrasi;
+
+  public function __construct()
+  {
+    $this->InformasiBiayaAdministrasi = nl2br("Informasi Biaya Administrasi Motor
+            ---------------------------------------
+            1. SWDKLLJ : Rp 80.000
+            2. Administrasi: Rp. 50.000
+            3. Pengesahan STNK: Rp. 25.000
+            4. Penerbitan STNK: Rp. 100.000
+            5. Penerbitan TNKB: Rp. 60.000
+        ");
+  }
+
+  public function getInformasiBiayaAdministrasi()
+  {
+    echo $this->InformasiBiayaAdministrasi;
+  }
+
+  public function getInformasiPresentasePajak()
+  {
+    return null;
+  }
+}
---
+++ package/perhitunganpajakkendaraanbermotor/src/InformasiPresentasePajakMotor.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace PerhitunganPajakKendaranBermotor;
+
+class InformasiPresentasePajakMotor implements Informasi
+{
+  private $InformasiPresentasePajak;
+
+  public function __construct()
+  {
+    $this->InformasiPresentasePajak = nl2br("Informasi Presentase Pajak Motor
+            ---------------------------------------
+            Tahun Pertama
+            1. BBN KB: 10% harga jual motor
+            2. PKB: 2% nilai jual motor (NJKB)
+            
+            Setelah Tahun Pertama
+            1. PKB: 2% nilai jual motor (NJKB)
+            
+            Pajak 5 Tahun
+            1. PKB: 2% nilai jual motor (NJKB)
+        ");
+  }
+
+  public function getInformasiPresentasePajak()
+  {
+    echo $this->InformasiPresentasePajak;
+  }
+
+  public function getInformasiBiayaAdministrasi()
+  {
+    return null;
+  }
+}
